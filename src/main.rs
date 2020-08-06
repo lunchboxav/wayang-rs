0226c9f435e1ee171115c7c11b29d7c11c305971
Adityo Pratomo <adityo@tetrate.io>
2020-08-06 18:26:03 +0700

    everything is in vector now
    Signed-off-by: Adityo Pratomo <adityo@tetrate.io>

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,33 @@
 use std::fs::File;
-use std::io::{self, BufRead};
-use std::path::Path;
+use std::io::prelude::*;
 
 fn main() {
-  if let Ok(lines) = read_lines("story/story.wyg") {
-    for line in lines {
-      if let Ok(ip) = line {
-          println!("{}", ip);
-      }
-    }
-  }  
-}
+  let mut data = String::new();
+  let mut f = File::open("story/story.wyg").expect("Unable to open file");
+  f.read_to_string(&mut data).expect("Unable to read data");
 
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where P: AsRef<Path>, {
-  let file = File::open(filename)?;
-  Ok(io::BufReader::new(file).lines())
+  let v: Vec<&str> = data.split("  \n").collect();
+
+  let v_scene: Vec<&str> = v[0].split("\n")
+    .into_iter()
+    .filter(|s| !s.contains("Scene"))
+    .filter(|s| !s.is_empty())
+    .collect();
+  let v_event: Vec<&str> = v[1].split("\n")
+    .into_iter()
+    .filter(|s| !s.contains("Event"))
+    .filter(|s| !s.is_empty())
+    .collect();
+  let v_branch: Vec<&str> = v[2].split("\n")
+    .into_iter()
+    .filter(|s| !s.contains("Branch"))
+    .filter(|s| !s.is_empty())
+    .collect();
+
+  println!("{:?}", v_scene);
+  println!("{:?}", v_event);
+  println!("{:?}", v_branch);
+
+
+
 }
