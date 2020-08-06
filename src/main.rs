65f6c02da3880f5ab76dcc43a42a82c168f1d589
Adityo Pratomo <adityo@tetrate.io>
2020-08-06 12:44:40 +0700

    implemented basic file reading
    Signed-off-by: Adityo Pratomo <adityo@tetrate.io>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::io::prelude::*;
+use std::fs::File;
+use std::path::Path;
+
 fn main() {
-    println!("Hello, world!");
+    let path = Path::new("story/story.wyg");
+    let display = path.display();
+
+    let mut file = match File::open(&path) {
+        Err(why) => panic!("couldn't open {}: {}", display, why),
+        Ok(file) => file,
+    };
+
+    let mut s = String::new();
+    match file.read_to_string(&mut s) {
+        Err(why) => panic!("couldn't read {}: {}", display, why),
+        Ok(_) => print!("{} contains:\n{}", display, s),
+    }
 }
